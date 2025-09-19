465c751ed98c78cd5d1285be4ea3ed57bdb09237
Gabrovez <gabrovez321@gmail.com>
2025-09-19 23:02:12 +0300

    Шаг 6. while

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            int[] numbers = [-1, -2, -3, 4, 5, -6, 0,
+            /*int[] numbers = [-1, -2, -3, 4, 5, -6, 0,
                 43, 56, -34, 12, 45, -102, 76, -45, 96, 
                 -4, -3, 5, 76, -6, -8, 2, 0, 5, -9];
             int res = 0;
@@ -12,6 +12,18 @@
             {
                 if (numbers[i] > 0)
                     res++;
+            }
+            Console.WriteLine($"Число элементов больше нуля: {res}");*/
+            int[] numbers = [-1, -2, -3, 4, 5, -6, 0,
+                43, 56, -34, 12, 45, -102, 76, -45, 96, 
+                -4, -3, 5, 76, -6, -8, 2, 0, 5, -9];
+            int res = 0;
+            int index = 0;
+            while (index < numbers.Length)
+            {
+                if (numbers[index] > 0)
+                    res++;
+                index++;
             }
             Console.WriteLine($"Число элементов больше нуля: {res}");
                
