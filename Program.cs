83380e2a8064847c90e96b9e6ef2060a8cc37c9f
Gabrovez <gabrovez321@gmail.com>
2025-09-19 23:15:48 +0300

    //Задание 1

--- Program.cs
+++ Program.cs
@@ -14,7 +14,7 @@
                     res++;
             }
             Console.WriteLine($"Число элементов больше нуля: {res}");*/
-            int[] numbers = [-1, -2, -3, 4, 5, -6, 0,
+            /*int[] numbers = [-1, -2, -3, 4, 5, -6, 0,
                 43, 56, -34, 12, 45, -102, 76, -45, 96, 
                 -4, -3, 5, 76, -6, -8, 2, 0, 5, -9];
             int res = 0;
@@ -25,7 +25,9 @@
                     res++;
                 index++;
             }
-            Console.WriteLine($"Число элементов больше нуля: {res}");
+            Console.WriteLine($"Число элементов больше нуля: {res}");*/
+            //Задание 1
+            string[] mas = ["sword", "shield", "potion", "sword", "potion", "potion" ];
                
         }
     }
