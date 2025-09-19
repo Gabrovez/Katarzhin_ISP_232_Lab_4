1636e4822b113c1cf8dac5dc8684ed00bce57ba3
Gabrovez <gabrovez321@gmail.com>
2025-09-19 22:54:50 +0300

    Шаг 6. for

--- Program.cs
+++ Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[] numbers = [-1, -2, -3, 4, 5, -6, 0,
+                43, 56, -34, 12, 45, -102, 76, -45, 96, 
+                -4, -3, 5, 76, -6, -8, 2, 0, 5, -9];
+            int res = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] > 0)
+                    res++;
+            }
+            Console.WriteLine($"Число элементов больше нуля: {res}");
+               
         }
     }
 }
